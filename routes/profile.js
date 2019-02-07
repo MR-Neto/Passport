7c6bb218fbdb61ceab8ae2cc35b4a257aee280cb
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-02-07 16:10:29 +0100

    Update User from profile view

--- routes/profile.js
+++ routes/profile.js
@@ -1,16 +1,40 @@
 const express = require('express');
+const User = require('../models/user');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('profile');
-  console.log(req.session.currentUser);
 });
 
-router.post('/profile', (req, res, next) => {
+router.post('/', (req, res, next) => {
   const { username, homeCountry } = req.body;
-  const { id } = req.session.currentUser;
+  const { _id } = req.session.currentUser;
+
+  if (username === '' || homeCountry === '') {
+    // res.render('partials/signup', {
+    //   errorMessage: 'Indicate a username and a password to sign up'
+    // });
+    return;
+  }
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        User.findByIdAndUpdate(_id, { username, homeCountry })
+          .then(() => {
+            res.redirect('/travellog');
+          })
+          .catch((err) => {
+            next(err);
+          });
+      } else {
+        // res.render('partials/signup', { errorMessage: "Incorrect Username or Password" });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
