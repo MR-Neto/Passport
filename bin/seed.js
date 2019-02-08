eb4145b40f5323062df0d942cee6546cc80c592f
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-02-08 16:14:56 +0100

    Countries API and Seed

--- bin/seed.js
+++ bin/seed.js
@@ -1,26 +1,6 @@
+const axios = require('axios');
 const mongoose = require('mongoose');
 const Country = require('../models/country');
-
-const countries = [
-  {
-    name: 'Portugal',
-  },
-  {
-    name: 'France',
-  },
-  {
-    name: 'Spain',
-  },
-  {
-    name: 'Italy',
-  },
-  {
-    name: 'Belgium',
-  },
-  {
-    name: 'Guatemala',
-  },
-];
 
 mongoose.connect('mongodb://localhost:27017/travelLog', { useNewUrlParser: true })
   .then(() => {
@@ -29,7 +9,23 @@ mongoose.connect('mongodb://localhost:27017/travelLog', { useNewUrlParser: true 
   })
   .then(() => {
     console.log('deleted');
-    return Country.insertMany(countries);
+    return axios.get('https://restcountries.eu/rest/v2/all?fields=name;capital;region;subregion;population;latlng;area;languages;flag;regionalBlocs');
+  })
+  .then((res) => {
+    console.log('accessed API');
+    const { data } = res;
+    const cleanData = data.map((country) => {
+      let lat = country.latlng[0] || 0;
+      let lng = country.latlng[1] || 0;
+      country.location = {
+        type: 'Point',
+        coordinates: [lng, lat],
+      };
+      delete country.latlng;
+      return country;
+    });
+
+    return Country.insertMany(cleanData);
   })
   .then(() => {
     console.log('loaded data');
