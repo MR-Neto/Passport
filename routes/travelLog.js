689c2919fee49dde68aa40922bb65b165285f564
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-02-14 16:30:50 +0100

    added comparison

--- routes/travelLog.js
+++ routes/travelLog.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Types;
 const axios = require('axios');
 const Country = require('../models/country');
 const User = require('../models/user');
@@ -102,11 +103,43 @@ router.get('/map', (req, res, next) => {
   Trip.find({ users: _id }).populate('countries.country')
     .then((trips) => {
       const countries = Statistics.uniqueArray(Statistics.flattenArray(trips));
+      console.log(countries);
       res.render('map', { countries, GMAPAPIKEY: process.env.GMAPAPIKEY });
     })
     .catch((error) => {
       next(error);
     });
+});
+
+router.get('/comparison', async (req, res, next) => {
+  const user = req.session.currentUser._id;
+
+  const tripsUser = await Trip.find({ users: user }).populate('countries.country');
+  const countriesUser = Statistics.uniqueArray(Statistics.flattenArray(tripsUser));
+  const countriesComparisonUser = [];
+
+  res.render('comparison', { countriesUser, countriesComparisonUser, GMAPAPIKEY: process.env.GMAPAPIKEY });
+});
+
+router.post('/comparison', async (req, res, next) => {
+  const user = req.session.currentUser._id;
+
+  const { nameComparisonUser } = req.body;
+
+  const tripsUser = await Trip.find({ users: user }).populate('countries.country');
+  console.log('Trips User: ', tripsUser);
+  const countriesUser = Statistics.uniqueArray(Statistics.flattenArray(tripsUser));
+  console.log('Countries User: ', countriesUser);
+  const comparisonUser = await User.find({ username: nameComparisonUser });
+  console.log('Comparison User ID: ', comparisonUser[0]._id);
+  const tripsComparisonUser = await Trip
+    .find({ users: comparisonUser[0]._id })
+    .populate('countries.country');
+  console.log('Trips Comparison User: ', tripsComparisonUser);
+  const countriesComparisonUser = Statistics.uniqueArray(Statistics.flattenArray(tripsComparisonUser));
+  console.log('Countries Comparison User: ', countriesComparisonUser);
+
+  res.render('comparison', { countriesUser, countriesComparisonUser, GMAPAPIKEY: process.env.GMAPAPIKEY });
 });
 
 router.get('/:id', (req, res, next) => {
@@ -132,7 +165,5 @@ router.post('/:id/delete', (req, res, next) => {
       next(error);
     });
 });
-
-
 
 module.exports = router;
