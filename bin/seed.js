b5cc91bb00bdf678540ba5c96ad5b40ee585d8db
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-02-15 11:28:25 +0100

    Seed clean countries names

--- bin/seed.js
+++ bin/seed.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 const axios = require('axios');
 const mongoose = require('mongoose');
 const Country = require('../models/country');
@@ -16,6 +16,26 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
     console.log('accessed API');
     const { data } = res;
     const cleanData = data.map((country) => {
+      if (country.name === 'Bolivia (Plurinational State of)') {
+        country.name = 'Bolivia';
+      }
+      if (country.name === 'Iran (Islamic Republic of)') {
+        country.name = 'Iran';
+      }
+      if (country.name === `Korea (Democratic People's Republic of)`) {
+        country.name = 'North Korea';
+      }
+      if (country.name === 'Korea (Republic of)') {
+        country.name = 'South Korea';
+      }
+      if (country.name === 'United States of America') {
+        country.name = 'United States';
+      }
+      if (country.name === 'United Kingdom of Great Britain and Northern Ireland') {
+        country.name = 'United Kingdom';
+      }
+
+
       let lat = country.latlng[0] || 0;
       let lng = country.latlng[1] || 0;
       country.location = {
